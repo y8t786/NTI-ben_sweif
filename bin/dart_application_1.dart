dac1384ea25be7afa3706355da44300cfef402db
y8t786 <youssefsaeed294@gmail.com>
2026-03-31 16:04:59 +0200

    first coment

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
+
+void main(List<String> arguments) {
+  List<int> numbers = [1, 2, 3, 4, 5];
+  List<String> names = ['Alice', 'Bob', 'Charlie'];
+  print('Numbers: $numbers');
+  print(names.length);
+  var num = {1, 2, 3};
+  num.toList();
+  //final Name = {'ahmed', 'amr'};
+  //Runes emoji = Runes('\u{1F600}');
+  //print('Emoji: ${String.fromCharCodes(emoji)}');
+  // print('Names:');
+  //String? name = stdin.readLineSync();
+  //print('Hello, $name!');
+  //print('Age: ');
+  //int? age = int.tryParse(stdin.readLineSync()!);
+  //print('You are $age years old.');
+}
