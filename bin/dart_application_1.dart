9ae84c991ccb83e88410d14a50e30c6487d1bd2d
y8t786 <youssefsaeed294@gmail.com>
2026-03-31 17:42:07 +0200

    hhhh

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -4,12 +4,12 @@ import 'package:dart_application_1/dart_application_1.dart'
     as dart_application_1;
 
 void main(List<String> arguments) {
-  List<int> numbers = [1, 2, 3, 4, 5];
-  List<String> names = ['Alice', 'Bob', 'Charlie'];
-  print('Numbers: $numbers');
-  print(names.length);
-  var num = {1, 2, 3};
-  num.toList();
+  // List<int> numbers = [1, 2, 3, 4, 5];
+  //List<String> names = ['Alice', 'Bob', 'Charlie'];
+  //print('Numbers: $numbers');
+  //print(names.length);
+  ///var num = {1, 2, 3};
+  //num.toList();
   //final Name = {'ahmed', 'amr'};
   //Runes emoji = Runes('\u{1F600}');
   //print('Emoji: ${String.fromCharCodes(emoji)}');
@@ -19,4 +19,15 @@ void main(List<String> arguments) {
   //print('Age: ');
   //int? age = int.tryParse(stdin.readLineSync()!);
   //print('You are $age years old.');
+  greet(name: 'Alice', age: 30);
+  print(describe('Alice', 'New York'));
+}
+
+void greet({String name = 'Guest', int age = 0}) {
+  print('Hello, $name!, You are $age years old.');
+}
+
+//////////////////////////
+String describe(String name, String city) {
+  return 'My name is $name and I live in $city.';
 }
